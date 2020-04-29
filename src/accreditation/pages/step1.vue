ab11eb5a4d7704cb15d99fae16a97bd329ce95f4
Murilo Angelo <muriloangelo10@gmail.com>
2020-04-29 09:04:04 -0300

    finishing accreditation first step
    Signed-off-by: Murilo Angelo <muriloangelo10@gmail.com>

--- src/accreditation/pages/step1.vue
+++ src/accreditation/pages/step1.vue
@@ -10,36 +10,76 @@ export default {
   },
   data() {
     return {
-      email: "",
-      cpf: "",
-      emailMessages: [
-        { key: "email", message: "Email inválido" },
-        { key: "required", message: "Este campo é obrigatório." },
-        { key: "isUnique", message: "Email em uso." }
-      ]
+
+      accredited: {
+        accreditationType: '0',
+        vet: false,
+        name: '',
+        cpf: '',
+        birthdate: '',
+        phone: '',
+        crmv: '',
+        crmv_uf: '',
+        credential_attributes: {
+          credential_type: 'client',
+          email: '',
+          password: '',
+          password_confirmation: '',
+        }
+      },
+      states: ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 
+        'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 
+        'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
+      ],
+      nameMessages: [{ key: "required", message: "Este campo é obrigatório. " },],
+      cpfMessages: [{ key: "required", message: "Este campo é obrigatório. " },],
+      birthdateMessages: [{ key: "required", message: "Este campo é obrigatório. " }, { key: "date", message: "Data inválida. " }],
+      phoneMessages: [{ key: "required", message: "Este campo é obrigatório. " },{ key: "minLength", message: "Número de telefone incompleto. " }],
+      emailMessages: [{ key: "required", message: "Este campo é obrigatório. " },{ key: "email", message: "Email inválido. " },{ key: "isUnique", message: "Email já cadastrado. " }],
+      passwordMessages: [{ key: "required", message: "Este campo é obrigatório. " },{ key: "minLength", message: "Senha deve conter no minimo 8 caracteres. " }],
+      passwordConfirmationMessages: [{ key: "required", message: "Este campo é obrigatório. " },{ key: "sameAsPassword", message: "Senha deve ser identica. " }],
+      crmvMessages: [{ key: "required", message: "Este campo é obrigatório. " }],
+      crmvUfMessages: [{ key: "required", message: "Este campo é obrigatório. " }],
     };
   },
   validations: {
-    cpf: {},
-    email: {
-      email,
-      required,
-      async isUnique(value) {
-        if (value === "" || !this.$v.email.email) return true;
-        if (this.$v.email.email) {
-          const response = await this.$api.post("clients/by_email", {
-            email: value
-          });
-          return !!response.data;
+    accredited: {
+      name: { required },
+      cpf: { required },
+      birthdate: {
+        required, 
+        date: async (value) => { 
+          if (value.length == 10) return moment(value, 'DD/MM/YYYY', true).isValid()
+          return true
         }
+      },
+      phone: { required, minLength: minLength(15) },
+      crmv: { required },
+      crmv_uf: { required },
+      credential_attributes: {
+        email: {
+          email,
+          required,
+          async isUnique(value) {
+            if (value === "" || !this.$v.accredited.credential_attributes.email.email) return true;
+            if (this.$v.accredited.credential_attributes.email.email) {
+              const response = await this.$api.post("clients/by_email", {
+                email: value
+              });
+              return !response.data;
+            }
+          }
+        },
+        password: { required, minLength: minLength(8) },
+        password_confirmation: { required, sameAsPassword: sameAs('password') },
       }
-    }
+    },
   },
   methods: {
     validate() {
-      this.$v.email.$touch();
-      var isValid = !this.$v.email.$invalid;
-      this.$emit("on-validate", { email }, isValid);
+      this.$v.accredited.$touch();
+      var isValid = !this.$v.accredited.$invalid;
+      this.$emit("on-validate", { accredited: this.accredited }, isValid);
       return isValid;
     }
   }
@@ -50,22 +90,143 @@ export default {
   <div>
     <form class="form-horizontal">
       <div class="row">
+        <div class="col-12">
+          <h4 class="font-size-15 mt-3 text-center">Como deseja se credenciar?</h4>
+        </div>
+        <div class="col-6 text-right">
+          <div class="custom-control custom-radio mb-2">
+            <input
+              v-model="accredited.accreditationType"
+              id="accreditationTypeCheck1"
+              type="radio"
+              class="custom-control-input"
+              value="0"
+            />
+            <label class="custom-control-label" for="accreditationTypeCheck1">
+              Estabelecimento
+            </label>
+          </div>
+        </div>
+        <div class="col-6 text-left">
+          <div class="custom-control custom-radio">
+            <input
+              v-model="accredited.accreditationType"
+              id="accreditationTypeCheck2"
+              type="radio"
+              class="custom-control-input"
+              value="1"
+            />
+            <label class="custom-control-label" for="accreditationTypeCheck2">
+              Veterinário autônomo
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
         <div class="col-12 col-md-6">
           <InputText
-            v-model="$v.email.$model"
-            label="Email"
+            v-model="$v.accredited.name.$model"
+            label="Nome"
             required
-            :validations="$v.email"
-            :errorMessages="emailMessages"
-            type="email"
+            :validations="$v.accredited.name"
+            :errorMessages="nameMessages"
           />
         </div>
         <div class="col-12 col-md-6">
           <InputText
-            v-model="$v.cpf.$model"
+            v-model="$v.accredited.cpf.$model"
             label="CPF"
+            required
+            :validations="$v.accredited.cpf"
+            :errorMessages="cpfMessages"
             type="tel"
             mask="###.###.###-##"
+          />
+        </div>
+        <div class="col-12 col-md-6">
+          <InputText
+            v-model="$v.accredited.birthdate.$model"
+            label="Nascimento"
+            required
+            :validations="$v.accredited.birthdate"
+            :errorMessages="birthdateMessages"
+            type="tel"
+            mask="##/##/####"
+          />
+        </div>
+        <div class="col-12 col-md-6">
+          <InputText
+            v-model="$v.accredited.phone.$model"
+            label="Telefone"
+            required
+            :validations="$v.accredited.phone"
+            :errorMessages="phoneMessages"
+            type="tel"
+            mask="(##) #####-####"
+          />
+        </div>
+        <hr>
+        <div class="col-12 col-md-6">
+          <InputText
+            v-model="$v.accredited.credential_attributes.email.$model"
+            label="Email"
+            required
+            :validations="$v.accredited.credential_attributes.email"
+            :errorMessages="emailMessages"
+            type="email"
+          />
+        </div>
+        <div class="col-12 col-md-3">
+          <InputText
+            v-model="$v.accredited.credential_attributes.password.$model"
+            label="Senha"
+            required
+            :validations="$v.accredited.credential_attributes.password"
+            :errorMessages="passwordMessages"
+            type="password"
+          />
+        </div>
+
+        <div class="col-12 col-md-3">
+          <InputText
+            v-model="$v.accredited.credential_attributes.password_confirmation.$model"
+            label="Confirmação de senha"
+            required
+            :validations="$v.accredited.credential_attributes.password_confirmation"
+            :errorMessages="passwordConfirmationMessages"
+            type="password"
+          />
+        </div>
+      </div>
+
+      <div class="row" v-if="accredited.accreditationType == 0">
+        <div class="col-12 text-center">
+          <b-form-group>
+            <label> Você atuará como veterinário no estabelecimento em questão? </label>
+            <b-form-checkbox v-model="accredited.vet" name="check-button" switch>
+              {{ accredited.vet ? 'Sim' : 'Não'}}
+            </b-form-checkbox>
+          </b-form-group>
+        </div>
+
+        <div class="col-6 col-md-3" v-if="accredited.vet">
+          <InputText
+            v-model="$v.accredited.crmv.$model"
+            label="CRMV"
+            required
+            :validations="$v.accredited.crmv"
+            :errorMessages="crmvMessages"
+          />
+        </div>
+
+        <div class="col-6 col-md-3" v-if="accredited.vet">
+          <InputText
+            v-model="$v.accredited.crmv_uf.$model"
+            label="UF"
+            required
+            :validations="$v.accredited.crmv_uf"
+            :errorMessages="crmvUfMessages"
           />
         </div>
       </div>
